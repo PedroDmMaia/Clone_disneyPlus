beed07647d94c8964641b2e06be8b84a79aa67dc
Pedro Maia <pedrodelmonico@hotmail.com>
2023-09-26 17:37:28 -0300

    adicionando o header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('[data-tab-button]');//selecionando o botão por atributo
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight; //pegando a altura da section hero
+
+    window.addEventListener('scroll', () => {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementoDoHeader();
+        } else {
+            exibeElementoDoHeader();
+        }
+    });
+
+    //seção de atrações, programação das abas
     buttons.forEach(button => {
         button.addEventListener('click', botao => {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,10 +27,22 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+
+    //seção FAQ, accordion
     questions.forEach(question => {
         question.addEventListener('click', abreOuFechaResposta); //por padrão o próprio elemento vai como argumento, nesse caso, o "question" que esta iterando.
     });
 });
+
+function ocultaElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreOuFechaResposta(question) {
     const classe = 'faq__questions__item--is-open';
